9247b28c281ec69b2adee8d6ad6c530cd1ab2974
koukurme-cyber <koukurme@gmail.com>
2026-09-20 20:58:45 +0300

    Add active Pause protection and launch shortcuts

--- app/src/main/java/ru/pauza/app/model/InstalledApp.kt
+++ app/src/main/java/ru/pauza/app/model/InstalledApp.kt
@@ -2,8 +2,15 @@ package ru.pauza.app.model
 
 import android.graphics.Bitmap
 
+enum class AppLaunchType {
+    PACKAGE,
+    PHONE,
+    MESSAGES,
+}
+
 data class InstalledApp(
     val label: String,
     val packageName: String,
     val icon: Bitmap?,
+    val launchType: AppLaunchType = AppLaunchType.PACKAGE,
 )
