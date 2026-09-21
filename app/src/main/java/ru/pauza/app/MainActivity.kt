99554544458be7e06d595427576fc7fca23ab98f
koukurme-cyber <koukurme@gmail.com>
2026-09-21 15:58:37 +0300

    Use dark Android navigation controls on light background

--- app/src/main/java/ru/pauza/app/MainActivity.kt
+++ app/src/main/java/ru/pauza/app/MainActivity.kt
@@ -1,6 +1,10 @@
 package ru.pauza.app
 
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.view.WindowInsetsController
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -14,6 +18,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        configureSystemNavigation()
 
         val store = PauseStore(this)
         val appsRepository = InstalledAppsRepository(this)
@@ -26,6 +31,34 @@ class MainActivity : ComponentActivity() {
                     appsRepository = appsRepository,
                     blocker = blocker,
                 )
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        configureSystemNavigation()
+        @Suppress("DEPRECATION")
+        overridePendingTransition(0, 0)
+    }
+
+    private fun configureSystemNavigation() {
+        window.navigationBarColor = Color.WHITE
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            window.isNavigationBarContrastEnforced = false
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.setSystemBarsAppearance(
+                WindowInsetsController.APPEARANCE_LIGHT_NAVIGATION_BARS,
+                WindowInsetsController.APPEARANCE_LIGHT_NAVIGATION_BARS
+            )
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            @Suppress("DEPRECATION")
+            run {
+                window.decorView.systemUiVisibility =
+                    window.decorView.systemUiVisibility or View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
             }
         }
     }
