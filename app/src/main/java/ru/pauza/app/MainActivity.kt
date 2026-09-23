53e9a6fd64608ac77f559df3f6ebeb4f3e00d15d
koukurme-cyber <koukurme@gmail.com>
2026-09-23 16:07:33 +0300

    Use device-owner blocker for strict Pause

--- app/src/main/java/ru/pauza/app/MainActivity.kt
+++ app/src/main/java/ru/pauza/app/MainActivity.kt
@@ -6,7 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import ru.pauza.app.data.InstalledAppsRepository
 import ru.pauza.app.data.PauseStore
-import ru.pauza.app.domain.AccessibilityPauseBlocker
+import ru.pauza.app.domain.DeviceOwnerPauseBlocker
 import ru.pauza.app.ui.PauseRoot
 import ru.pauza.app.ui.theme.PauseTheme
 
@@ -17,7 +17,7 @@ class MainActivity : ComponentActivity() {
 
         val store = PauseStore(this)
         val appsRepository = InstalledAppsRepository(this)
-        val blocker = AccessibilityPauseBlocker(this)
+        val blocker = DeviceOwnerPauseBlocker(this)
 
         setContent {
             PauseTheme {
