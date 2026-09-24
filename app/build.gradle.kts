e2f5e9c58a2d6f74ed02681c1ef2d815513a44bf
koukurme-cyber <koukurme@gmail.com>
2026-09-24 12:58:15 +0300

    Version Pause 0.8.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         minSdk = 26
         targetSdk = 36
-        versionCode = 54
-        versionName = "0.8.4"
+        versionCode = 55
+        versionName = "0.8.5"
     }
 
     signingConfigs {
