0416f795316ffaee24a951e975decb6a5834a831
koukurme-cyber <koukurme@gmail.com>
2026-09-25 04:49:03 +0300

    Version Pause 0.8.13

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         minSdk = 26
         targetSdk = 36
-        versionCode = 62
-        versionName = "0.8.12"
+        versionCode = 63
+        versionName = "0.8.13"
     }
 
     signingConfigs {
